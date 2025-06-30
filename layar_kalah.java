1075ea77a2f4a9fcc60811f634cc4ff568fe275e
edwinronaltamamabrur <140263599+Ronaltama@users.noreply.github.com>
2025-06-30 13:07:35 +0700

    update gambar

---
+++ layar_kalah.java
@@ -0,0 +1,21 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * Write a description of class layar_kalah here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class layar_kalah extends World
+{
+
+    /**
+     * Constructor for objects of class layar_kalah.
+     * 
+     */
+    public layar_kalah()
+    {    
+        // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
+        super(600, 400, 1); 
+    }
+}
